f52d06cc34e694d37ec9defb50cec77dd3f6fd08
Junaid gazi <mojunedgazi@gmail.com>
2023-06-21 12:05:42 +0530

    first commit

---
+++ break&continue.js
@@ -0,0 +1,11 @@
+//The Break Statement
+for (let i = 0; i < 10; i++) {
+    if (i === 3) { break; }
+    text += "The number is " + i + "<br>";
+  }
+
+//The Continue Statement
+for (let i = 0; i < 10; i++) {
+    if (i === 3) { continue; }
+    text += "The number is " + i + "<br>";
+  }
